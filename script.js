3aaf560ae4656d5144a49ab5e8f3a5a182b44eda
yirklum <yirklum@aol.com>
2017-07-05 22:46:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,80 +2,6 @@ $(document).ready(function(){
 	
 	console.log("get document ready");
 
-	/* Homepage Categories */
-
-	$("#show-thisjustin").click(function () {
-		$("#thisjustin").show();
-		$(this).hide();
-		$("#hide-thisjustin").show();
-	});
-	
-	$("#hide-thisjustin").click(function () {
-		$("#thisjustin").hide();
-		$(this).hide();
-		$("#show-thisjustin").show();
-	});
-
-	$("#show-thewayitwas").click(function () {
-		$("#thewayitwas").show();
-		$(this).hide();
-		$("#hide-thewayitwas").show();
-	});
-	
-	$("#hide-thewayitwas").click(function () {
-		$("#thewayitwas").hide();
-		$(this).hide();
-		$("#show-thewayitwas").show();
-	});
-
-	$("#show-moreonthestory").click(function () {
-		$("#moreonthestory").show();
-		$(this).hide();
-		$("#hide-moreonthestory").show();
-	});
-	
-	$("#hide-moreonthestory").click(function () {
-		$("#moreonthestory").hide();
-		$(this).hide();
-		$("#show-moreonthestory").show();
-	});
-
-	$("#show-newsfeed").click(function () {
-		$("#newsfeed").show();
-		$(this).hide();
-		$("#hide-newsfeed").show();
-	});
-	
-	$("#hide-newsfeed").click(function () {
-		$("#newsfeed").hide();
-		$(this).hide();
-		$("#show-newsfeed").show();
-	});
-
-	$("#show-talkingheads").click(function () {
-		$("#talkingheads").show();
-		$(this).hide();
-		$("#hide-talkingheads").show();
-	});
-	
-	$("#hide-talkingheads").click(function () {
-		$("#talkingheads").hide();
-		$(this).hide();
-		$("#show-talkingheads").show();
-	});
-
-	$("#show-newsgatheringtools").click(function () {
-		$("#newsgatheringtools").show();
-		$(this).hide();
-		$("#hide-newsgatheringtools").show();
-	});
-	
-	$("#hide-newsgatheringtools").click(function () {
-		$("#newsgatheringtools").hide();
-		$(this).hide();
-		$("#show-newsgatheringtools").show();
-	});
-
 	/* Contact Forms */
 
 	$("form").submit(function(event){
@@ -111,14 +37,16 @@ $(document).ready(function(){
 
     });
 
+	/*Memorabilia*/
+
     // Get the modal
 	var modal = document.getElementById('shatteredGlassModal');
 
 	// Get the image and insert it inside the modal - use its "alt" text as a caption
-	var img = document.getElementById('shatteredGlass');
+	var aimg = document.getElementById('shatteredGlass');
 	var modalImg = document.getElementById("shatteredGlassImage");
 	var captionText = document.getElementById("shatteredGlassCaption");
-	img.onclick = function(){
+	aimg.onclick = function(){
 	    modal.style.display = "block";
 	    modalImg.src = this.src;
 	    captionText.innerHTML = this.alt;
@@ -131,5 +59,6 @@ $(document).ready(function(){
 	span.onclick = function() { 
 	  modal.style.display = "none";
 	}
-
+	
 });
+
